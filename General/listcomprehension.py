cd45aac54c1a11d79e695dc296131029ce0b5c7b
mukulrawat1986 <mukulsrawat@gmail.com>
2019-02-09 15:39:28 +0530

    list comprehension with conditional logic

--- General/listcomprehension.py
+++ General/listcomprehension.py
@@ -10,3 +10,12 @@ friends = ['matt', 'mona', 'michael']
 
 # operate on the string
 print([friend[0].upper() + friend[1:] for friend in friends])
+
+# list comprehension with conditional logic
+nums = range(1,7)
+
+evens = [num for num in nums if num % 2 == 0]
+odds = [num for num in nums if num % 2 != 0]
+
+# when using else keyword
+print([num*2 if num % 2 == 0 else num/2 for num in nums])
